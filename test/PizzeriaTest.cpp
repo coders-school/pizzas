317e8ebf6fb81cfbd9be1d79eb38a423b001a1ec
m-kop <m.kopertowski@epicspace.eu>
2020-09-09 11:35:20 +0200

    add TimerMock struct + EXPECT_CALL in tests

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include "mocks/PizzaMock.hpp"
-#include "mocks/DummyTimer.hpp"
+#include "mocks/TimerMock.hpp"
 #include "Pizzeria.hpp"
 #include "Margherita.hpp"
 #include "Funghi.hpp"
@@ -12,8 +12,8 @@ using namespace ::testing;
 struct PizzeriaTest : public ::testing::Test
 {
 public:
-    DummyTimer timer;
-    Pizzeria pizzeria = Pizzeria("dummyName", timer); 
+    StrictMock<TimerMock> timerMock{};
+    Pizzeria pizzeria = Pizzeria("dummyName", timerMock); 
 };
 
 
@@ -34,7 +34,8 @@ TEST_F(PizzeriaTest, bakeDummyPizza)
 {
     // Given
     Pizzas pizzas = {new PizzaDummy{}};
-
+    EXPECT_CALL(timerMock, sleep_for(minutes(0))).Times(1);
+    
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
     pizzeria.bakePizzas(orderId);
@@ -44,7 +45,8 @@ TEST_F(PizzeriaTest, completeOrderWithStubPizza)
 {
     // Given
     Pizzas pizzas = {new PizzaStub{"STUB"}};
-
+    EXPECT_CALL(timerMock, sleep_for(minutes(1))).Times(1);
+    
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
     pizzeria.bakePizzas(orderId);
@@ -54,16 +56,20 @@ TEST_F(PizzeriaTest, completeOrderWithStubPizza)
 TEST_F(PizzeriaTest, calculatePriceForPizzaMock)
 {   
     // Given
-    PizzaMock* mock = new PizzaMock{};
-    Pizzas pizzas = {mock};
-    EXPECT_CALL(*mock, getPrice()).WillOnce(Return(40.0));
-    
+    PizzaMock mock{};
+    Pizzas pizzas = {&mock};
+    EXPECT_CALL(mock, getPrice()).WillOnce(Return(40.0));
+    EXPECT_CALL(mock, getName()).WillOnce(Return(""));
+    EXPECT_CALL(mock, getBakingTime()).WillOnce(Return(minutes(0)));
+    EXPECT_CALL(timerMock, sleep_for(minutes(0))).Times(1);
+
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
+    pizzeria.bakePizzas(orderId);
+    pizzeria.completeOrder(orderId);
     auto price = pizzeria.calculatePrice(orderId);
 
     // Then
     ASSERT_EQ(40, price);
 
-    delete mock;
 }
---
+++ test/mocks/TimerMock.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Timer.hpp"
+#include "gmock/gmock.h"
+
+struct TimerMock : public Timer {
+    MOCK_METHOD(void, sleep_for, (minutes), (override));    
+};
+
