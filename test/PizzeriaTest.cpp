e9a6894e0409b229bd87fcb202cacb985ea58df5
mtalma <talmamateusz@gmail.com>
2020-09-17 21:18:56 +0200

    Added test with order three pizzas

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -10,6 +10,15 @@
 using namespace std;
 using namespace ::testing;
 
+namespace PizzasConstValues {
+const std::string strictMockPizzaName = "strictMockPizzaName";
+const std::string niceMockPizzaName = "niceMockPizzaName";
+const minutes strictMockPizzaBakingTime = minutes(29);
+const minutes niceMockPizzaBakingTime = minutes(2);
+const double strictMockPizzaPrice = 23.2;
+const double niceMockPizzaPrice = 33.1;
+}  // namespace PizzasConstValues
+
 struct PizzeriaTest : public ::testing::Test {
 public:
     StrictMock<TimerMock> timerMock;
@@ -18,7 +27,9 @@ public:
 
 TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
     // Given
-    Pizzas pizzas = {new Margherita{25.0}, new Funghi{30.0}};
+    std::unique_ptr<Margherita> margherita = std::make_unique<Margherita>(25.0);
+    std::unique_ptr<Funghi> funghi = std::make_unique<Funghi>(30.0);
+    Pizzas pizzas = {margherita.get(), funghi.get()};
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
@@ -30,7 +41,8 @@ TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
 
 TEST_F(PizzeriaTest, bakeDummyPizza) {
     // Given
-    Pizzas pizzas = {new PizzaDummy{}};
+    std::unique_ptr<PizzaDummy> pizzaDummy = std::make_unique<PizzaDummy>();
+    Pizzas pizzas = {pizzaDummy.get()};
     EXPECT_CALL(timerMock, sleepFor(_));
 
     // When
@@ -40,7 +52,8 @@ TEST_F(PizzeriaTest, bakeDummyPizza) {
 
 TEST_F(PizzeriaTest, completeOrderWithStubPizza) {
     // Given
-    Pizzas pizzas = {new PizzaStub{"STUB"}};
+    std::unique_ptr<PizzaStub> pizzaStub = std::make_unique<PizzaStub>("STUB");
+    Pizzas pizzas = {pizzaStub.get()};
     EXPECT_CALL(timerMock, sleepFor(_));
 
     // When
@@ -63,4 +76,31 @@ TEST_F(PizzeriaTest, calculatePriceForPizzaMock) {
     ASSERT_EQ(40, price);
 
     delete mock;
+}
+
+TEST_F(PizzeriaTest, orderTwoPizzaMockAndOnePizzaStub) {
+    std::unique_ptr<PizzaStub> stubPizza = std::make_unique<PizzaStub>("StubPizza");
+    StrictMock<PizzaMock> strictMockPizza{};
+    NiceMock<PizzaMock> niceMockPizza{};
+
+    Pizzas pizzas = {stubPizza.get(), &strictMockPizza, &niceMockPizza};
+
+    EXPECT_CALL(strictMockPizza, getName()).WillRepeatedly(Return(PizzasConstValues::strictMockPizzaName));
+    EXPECT_CALL(strictMockPizza, getBakingTime()).WillRepeatedly(Return(PizzasConstValues::strictMockPizzaBakingTime));
+    EXPECT_CALL(strictMockPizza, getPrice()).WillRepeatedly(Return(PizzasConstValues::strictMockPizzaPrice));
+    EXPECT_CALL(niceMockPizza, getName()).WillRepeatedly(Return(PizzasConstValues::niceMockPizzaName));
+    EXPECT_CALL(niceMockPizza, getBakingTime()).WillRepeatedly(Return(PizzasConstValues::niceMockPizzaBakingTime));
+    EXPECT_CALL(niceMockPizza, getPrice()).WillRepeatedly(Return(PizzasConstValues::strictMockPizzaPrice));
+    EXPECT_CALL(timerMock, sleepFor(stubPizza->getBakingTime()));
+    EXPECT_CALL(timerMock, sleepFor(PizzasConstValues::strictMockPizzaBakingTime));
+    EXPECT_CALL(timerMock, sleepFor(PizzasConstValues::niceMockPizzaBakingTime));
+
+    auto orderId = pizzeria.makeOrder(pizzas);
+    pizzeria.bakePizzas(orderId);
+    pizzeria.completeOrder(orderId);
+    auto price = pizzeria.calculatePrice(orderId);
+
+    ASSERT_EQ(66.3, PizzasConstValues::strictMockPizzaPrice +
+                        PizzasConstValues::niceMockPizzaPrice +
+                        stubPizza->getPrice());
 }
