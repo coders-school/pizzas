0e08001e85019e1c80c3d9b778b82b446e1e91af
MonikaZ <monika.zelechowska@o2.pl>
2020-09-17 19:25:19 +0200

    Adding timer class

--- src/Pizza.hpp
+++ src/Pizza.hpp
@@ -1,15 +1,13 @@
 #pragma once
+#include <chrono>
 #include <list>
 #include <set>
-#include <chrono>
 #include <string>
+#include "Timer.hpp"
 
-using minutes = std::chrono::seconds;   // To simulate time flow ;)
-
-class Pizza
-{
+class Pizza {
 public:
-    Pizza(std::string const & name, double price, minutes bakingTime);
+    Pizza(std::string const& name, double price, minutes bakingTime);
     virtual ~Pizza() = default;
     virtual std::string getName() const;
     virtual double getPrice() const;
--- test/mocks/PizzaMock.hpp
+++ test/mocks/PizzaMock.hpp
@@ -1,33 +1,34 @@
 #include "Pizza.hpp"
 #include "gmock/gmock.h"
 
-struct PizzaDummy : public Pizza
-{
-    PizzaDummy() : Pizza("dummy", 0.0, minutes(0)) {}
+struct PizzaDummy : public Pizza {
+    PizzaDummy()
+        : Pizza("dummy", 0.0, minutes(0)) {}
     std::string getName() const override { return "dummy"; }
     double getPrice() const override { return 0.0; }
     minutes getBakingTime() const override { return minutes(0); }
 };
 
-struct PizzaStub : public Pizza
-{
-    PizzaStub(std::string name) : Pizza(name, 0.0, minutes(0)) {}
+struct PizzaStub : public Pizza {
+    PizzaStub(std::string name)
+        : Pizza(name, 0.0, minutes(0)) {}
     std::string getName() const override { return Pizza::getName(); }
-    double getPrice() const override
-    {
+    double getPrice() const override {
         if (getName() == "stub")
             return 0.0;
-        else 
+        else
             return 10.0;
     }
     minutes getBakingTime() const override { return minutes(1); }
 };
 
-struct PizzaMock : public Pizza
-{
-    PizzaMock() : Pizza("mock", 0.0, minutes(0)) {} // constructors should be avoided. Interface is advised
-    MOCK_CONST_METHOD0(getName, std::string());
-    MOCK_CONST_METHOD0(getPrice, double());
-    MOCK_CONST_METHOD0(getBakingTime, minutes());
+struct PizzaMock : public Pizza {
+    PizzaMock()
+        : Pizza("mock", 0.0, minutes(0)) {}  // constructors should be avoided. Interface is advised
+    //MOCK_CONST_METHOD0(getName, std::string());
+    MOCK_METHOD(std::string, getName, (), (const, override));
+    //MOCK_CONST_METHOD0(getPrice, double());
+    MOCK_METHOD(double, getPrice, (), (const, override));
+    //MOCK_CONST_METHOD0(getBakingTime, minutes());
+    MOCK_METHOD(minutes, getBakingTime, (), (const, override));
 };
-
