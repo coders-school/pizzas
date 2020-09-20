4fd704a01578e024b7041d17fc1b1d7de30b668d
Vicx95 <wiktor.zyla31@gmail.com>
2020-09-20 21:25:13 +0200

    Implement time mock

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -1,18 +1,19 @@
 #include <gtest/gtest.h>
 #include <string>
-#include "mocks/DummyTimer.hpp"
 #include "Funghi.hpp"
 #include "Margherita.hpp"
 #include "Pizzeria.hpp"
+#include "mocks/DummyTimer.hpp"
 #include "mocks/PizzaMock.hpp"
+#include "mocks/TimeMock.hpp"
 
 using namespace std;
 using namespace ::testing;
 
 struct PizzeriaTest : public ::testing::Test {
 public:
-    DummyTimer dt;
-    Pizzeria pizzeria = Pizzeria("dummyName", dt);
+    StrictMock<TimeMock> tm;
+    Pizzeria pizzeria = Pizzeria("dummyName", tm);
 };
 
 TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
@@ -30,6 +31,7 @@ TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55) {
 TEST_F(PizzeriaTest, bakeDummyPizza) {
     // Given
     Pizzas pizzas = {new PizzaDummy{}};
+    EXPECT_CALL(tm, sleep_for).Times(1);
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
@@ -39,6 +41,7 @@ TEST_F(PizzeriaTest, bakeDummyPizza) {
 TEST_F(PizzeriaTest, completeOrderWithStubPizza) {
     // Given
     Pizzas pizzas = {new PizzaStub{"STUB"}};
+    EXPECT_CALL(tm, sleep_for).Times(1);
 
     // When
     auto orderId = pizzeria.makeOrder(pizzas);
