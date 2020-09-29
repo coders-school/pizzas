dfa9d76c70ee2380a351ecc0001d309e12bcfb16
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-09-29 17:53:04 +0100

    Remove magic values and add constexpr instead

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -14,6 +14,10 @@ constexpr minutes mockBakingTime = minutes(10);
 constexpr minutes strictBakingTime = minutes(12);
 constexpr minutes niceBakingTime = minutes(16);
 
+constexpr double mockPizzaPrice = 40;
+constexpr double strictMockPizzaPrice = 30;
+constexpr double niceMockPizzaPrice = 35;
+
 struct PizzeriaTest : public ::testing::Test {
 public:
     StrictMock<MockTimer> mockTimer;
@@ -62,7 +66,7 @@ TEST_F(PizzeriaTest, calculatePriceForPizzaMock)
     NaggyMock<PizzaMock>* mock = new NaggyMock<PizzaMock>{};
     Pizzas pizzas = {mock};
     EXPECT_CALL(*mock, getName()).WillOnce(Return("naggy_mock"));
-    EXPECT_CALL(*mock, getPrice()).WillOnce(Return(40.0));
+    EXPECT_CALL(*mock, getPrice()).WillOnce(Return(mockPizzaPrice));
     EXPECT_CALL(*mock, getBakingTime).WillOnce(Return(mockBakingTime));
     EXPECT_CALL(mockTimer, sleepFor(mockBakingTime)).Times(1);
 
@@ -72,7 +76,7 @@ TEST_F(PizzeriaTest, calculatePriceForPizzaMock)
     auto price = pizzeria.calculatePrice(orderId);
 
     // Then
-    ASSERT_EQ(40, price);
+    ASSERT_EQ(mockPizzaPrice, price);
 
     delete mock;
 }
@@ -87,11 +91,11 @@ TEST_F(PizzeriaTest, orderOneStubAndTwoMocks) {
 
     EXPECT_CALL(*strictMockPizza, getName()).WillOnce(Return("scrict_mock"));
     EXPECT_CALL(*strictMockPizza, getBakingTime()).WillOnce(Return(strictBakingTime));
-    EXPECT_CALL(*strictMockPizza, getPrice()).WillOnce(Return(30.0));
+    EXPECT_CALL(*strictMockPizza, getPrice()).WillOnce(Return(strictMockPizzaPrice));
 
     EXPECT_CALL(*niceMockPizza, getName()).WillOnce(Return("nice_mock"));
     EXPECT_CALL(*niceMockPizza, getBakingTime()).WillOnce(Return(niceBakingTime));
-    EXPECT_CALL(*niceMockPizza, getPrice()).WillOnce(Return(35.0));
+    EXPECT_CALL(*niceMockPizza, getPrice()).WillOnce(Return(niceMockPizzaPrice));
 
     EXPECT_CALL(mockTimer, sleepFor(stubPizza->getBakingTime())).Times(1);
     EXPECT_CALL(mockTimer, sleepFor(strictBakingTime)).Times(1);
@@ -104,7 +108,7 @@ TEST_F(PizzeriaTest, orderOneStubAndTwoMocks) {
     auto price = pizzeria.calculatePrice(orderId);
 
     // Then
-    ASSERT_EQ(65, price);
+    ASSERT_EQ(strictMockPizzaPrice + niceMockPizzaPrice, price);
 
     delete stubPizza;
     delete strictMockPizza;
