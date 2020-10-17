952adaad004c792f078efcbbfbe253e647fbdc2e
damixiorek <damixiorek@gmail.com>
2020-10-17 14:18:15 +0200

    add thread timer and implement

---
+++ src/ThreadTimer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Timer.hpp"
+
+#include <thread>
+
+class ThreadTimer : Timer {
+public:
+    void sleep(minutes duration)
+    {
+        std::this_thread::sleep_for(duration);
+    }
+};
