b3eb0c5fc8de251cf283f1ba56da2aa543135bb4
damixiorek <damixiorek@gmail.com>
2020-10-17 19:15:47 +0200

    mocks and stubs in duplicated main function - testing

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -15,6 +15,10 @@ public:
     Pizzeria pizzeria = Pizzeria("dummyName", mockTimer);
 };
 
+struct PizzeriaTestMain : public ::testing::Test {
+    StrictMock<MockTimer> mockTimer;
+};
+
 TEST_F(PizzeriaTest, priceForMargherita25AndFunghi30ShouldBe55)
 {
     // Given
@@ -66,4 +70,37 @@ TEST_F(PizzeriaTest, calculatePriceForPizzaMock)
     ASSERT_EQ(40, price);
 
     delete mock;
+}
+
+TEST_F(PizzeriaTestMain, shouldDuplicateMain)
+{
+    constexpr minutes funghiBakingTime = minutes(4);
+    constexpr minutes margheritaBakingTime = minutes(3);
+
+    constexpr double cheaperPizza = 10.99;
+    constexpr double moreExpensivePizza = 250.0;
+
+    NiceMock<PizzaMock> niceMockPizza{};
+    StrictMock<PizzaMock> strictMockPizza{};
+
+    Pizzeria bravo("Bravo Pizza", mockTimer);
+    Pizzas pizzas = {new PizzaStub("Stub"), new PizzaStub("Stub2"), new PizzaStub("Stub3"), &niceMockPizza, &strictMockPizza};
+
+    EXPECT_CALL(mockTimer, sleep_for(minutes(1))).Times(3);
+    
+    EXPECT_CALL(niceMockPizza, getPrice()).WillOnce(Return(cheaperPizza));
+    EXPECT_CALL(niceMockPizza, getName()).WillOnce(Return("Special Margherita With More Cheese"));
+    EXPECT_CALL(niceMockPizza, getBakingTime()).WillOnce(Return(margheritaBakingTime));
+    EXPECT_CALL(mockTimer, sleep_for(margheritaBakingTime)).Times(1);
+
+    EXPECT_CALL(strictMockPizza, getPrice()).WillOnce(Return(moreExpensivePizza));
+    EXPECT_CALL(strictMockPizza, getName()).WillOnce(Return("Special Funghi With Olives"));
+    EXPECT_CALL(strictMockPizza, getBakingTime()).WillOnce(Return(funghiBakingTime));
+    EXPECT_CALL(mockTimer, sleep_for(funghiBakingTime)).Times(1);
+
+
+    auto orderId = bravo.makeOrder(pizzas);
+    auto price = bravo.calculatePrice(orderId);
+    bravo.bakePizzas(orderId);
+    bravo.completeOrder(orderId);
 }
