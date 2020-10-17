97f1fac2e40347b55fbf915b9fc6d98449c5e794
damixiorek <damixiorek@gmail.com>
2020-10-17 13:32:45 +0200

    add else with throw exeptions

--- src/Pizzeria.cpp
+++ src/Pizzeria.cpp
@@ -1,13 +1,13 @@
 #include "Pizzeria.hpp"
-#include <numeric>
 #include <algorithm>
-#include <thread>
 #include <iostream>
+#include <numeric>
+#include <thread>
 
-Pizzeria::Pizzeria(std::string const & name)
-    : name_(name)
-    , orders_()
-{}
+Pizzeria::Pizzeria(std::string const& name)
+    : name_(name), orders_()
+{
+}
 
 int Pizzeria::makeOrder(Pizzas pizzas)
 {
@@ -18,56 +18,50 @@ int Pizzeria::makeOrder(Pizzas pizzas)
 
 double Pizzeria::calculatePrice(int orderId)
 {
-    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto & order)
-    {
+    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto& order) {
         return std::get<0>(order) == orderId;
     });
-    if (order != orders_.end())
-    {
+    if (order != orders_.end()) {
         std::get<Status>(*order) = Status::Paid;
         auto pizzas = std::get<Pizzas>(*order);
         return std::accumulate(pizzas.begin(),
                                pizzas.end(),
                                0.0,
-                               [](double previousSum, Pizza *pizza)
-                               {
+                               [](double previousSum, Pizza* pizza) {
                                    return previousSum + pizza->getPrice();
                                });
     }
-    throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
+    else {
+        throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
+    }
 }
 
 void Pizzeria::bakePizzas(int orderId)
 {
-    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto & order)
-    {
+    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto& order) {
         return std::get<0>(order) == orderId;
     });
-    if (order != orders_.end())
-    {
+    if (order != orders_.end()) {
         auto pizzas = std::get<Pizzas>(*order);
-        for (const auto & pizza : pizzas)
-        {
+        for (const auto& pizza : pizzas) {
             std::cout << "Baking " << pizza->getName() << std::endl;
             std::this_thread::sleep_for(pizza->getBakingTime());
         }
         std::get<Status>(*order) = Status::Baked;
     }
-    throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
+    else {
+        throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
+    }
 }
 
 void Pizzeria::completeOrder(int orderId)
 {
-    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto & order)
-    {
+    auto order = std::find_if(orders_.begin(), orders_.end(), [orderId](const auto& order) {
         return std::get<0>(order) == orderId;
     });
-    if (order != orders_.end())
-    {
+    if (order != orders_.end()) {
         std::cout << "Order " << orderId << " completed" << std::endl;
         std::get<Status>(*order) = Status::Completed;
     }
     throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
 }
-
-
