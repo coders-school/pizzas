ed3c10e1a7210685c7b2a51b1e9d9529f5f7ee1f
lukasz-kukulka <qqla83@gmail.com>
2021-09-11 14:17:46 +0100

    Third error

--- src/Pizzeria.cpp
+++ src/Pizzeria.cpp
@@ -67,8 +67,9 @@ void Pizzeria::completeOrder(int orderId)
     {
         std::cout << "Order " << orderId << " completed" << std::endl;
         std::get<Status>(*order) = Status::Completed;
+    } else {
+        throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
     }
-    throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
 }
 
 
