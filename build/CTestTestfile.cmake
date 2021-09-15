47d5914149a7cd771f86723dc81f71cc9bdeb4fe
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-09-15 14:28:26 +0200

    delete useless things

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linux/workspace/pizzas
-# Build directory: /home/linux/workspace/pizzas/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PizzasTests "/home/linux/workspace/pizzas/build/Pizzas-ut")
-set_tests_properties(PizzasTests PROPERTIES  _BACKTRACE_TRIPLES "/home/linux/workspace/pizzas/CMakeLists.txt;66;add_test;/home/linux/workspace/pizzas/CMakeLists.txt;0;")
-subdirs("googletest-build")
