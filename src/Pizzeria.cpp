a70fbe0d6b9e15a71b0dc2c35ac032b7f3695d89
Aleksiiej <aleksander.ramut@wp.pl>
2022-03-10 11:27:11 +0100

    returns in bakePizzas and completeOrder added

--- src/Pizzeria.cpp
+++ src/Pizzeria.cpp
@@ -52,6 +52,7 @@ void Pizzeria::bakePizzas(int orderId)
             std::this_thread::sleep_for(pizza->getBakingTime());
         }
         std::get<Status>(*order) = Status::Baked;
+        return;
     }
     throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
 }
@@ -66,6 +67,7 @@ void Pizzeria::completeOrder(int orderId)
     {
         std::cout << "Order " << orderId << " completed" << std::endl;
         std::get<Status>(*order) = Status::Completed;
+        return;
     }
     throw std::invalid_argument("Order with id: " + std::to_string(orderId) + "not found");
 }
