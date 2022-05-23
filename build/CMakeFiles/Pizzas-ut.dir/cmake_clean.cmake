913d7f2db923e3c4311de9a4951fcf3c7a27670d
Sl0mek <andrzejslomkapraca@gmail.com>
2022-05-23 08:16:43 +0200

    Add build dir

---
+++ build/CMakeFiles/Pizzas-ut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pizzas-ut.dir/test/PizzeriaTest.cpp.o"
+  "Pizzas-ut"
+  "Pizzas-ut.pdb"
+  "Pizzas-ut[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pizzas-ut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrzej/Desktop/Pizza/pizzas
+# Build directory: /home/andrzej/Desktop/Pizza/pizzas/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/andrzej/Desktop/Pizza/pizzas/build/Pizzas-ut[1]_include.cmake")
