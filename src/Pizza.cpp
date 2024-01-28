469e46f455e488873dbdbd3409801645729b38e2
JamJestJam <marekti012@gmail.com>
2024-01-28 17:58:24 +0100

    Test

--- src/Pizza.cpp
+++ src/Pizza.cpp
@@ -1,23 +1,10 @@
 #include "Pizza.hpp"
 
-Pizza::Pizza(std::string const & name, double price, minutes bakingTime)
-    : name_(name)
-    , price_(price)
-    , bakingTime_(bakingTime)
-{}
+Pizza::Pizza(std::string const &name, double price, minutes bakingTime)
+    : name_(name), price_(price), bakingTime_(bakingTime) {}
 
-std::string Pizza::getName() const
-{
-    return name_;
-}
+std::string Pizza::getName() const { return name_; }
 
-double Pizza::getPrice() const
-{
-    return 30.0;
-}
+double Pizza::getPrice() const { return price_; }
 
-minutes Pizza::getBakingTime() const
-{
-    return bakingTime_;
-}
-
+minutes Pizza::getBakingTime() const { return bakingTime_; }
