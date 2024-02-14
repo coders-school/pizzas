6a89a563fc2b4f1177bb80351c36aabe8285a25e
Marcin Pawłowski <mnl@spyro-soft.com>
2024-02-14 22:31:33 +0100

    Fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 #include "Funghi.hpp"
 #include "Pizzeria.hpp"
 
+#include <iostream>
+
 int main()
 {
     Pizzeria bravo("Bravo Pizza");
     Pizzas pizzas = {new Margherita{25.0}, new Funghi{30.0}};
 
     auto orderId = bravo.makeOrder(pizzas);
-    /*auto price = */bravo.calculatePrice(orderId);
+    auto price = bravo.calculatePrice(orderId);
     bravo.bakePizzas(orderId);
     bravo.completeOrder(orderId);
+
+    std::cout << "Price: " << price << std::endl;
 
     return 0;
 }
